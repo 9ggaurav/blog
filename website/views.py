d1fa4587414647d713418d5ca850e8f95d42599f
Gaurav Raut <zgaurav713@gmail.com>
2025-03-09 15:38:36 +0530

    auth and security

--- website/views.py
+++ website/views.py
@@ -1,20 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
 @views.route("/home")
 @views.route("/")
+@login_required
 def home():
     return render_template("home.html")
 
-@views.route("/login")
-def login():
-    return render_template("login.html")
-
-@views.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-@views.route("/logout")
-def logout():
-    return redirect(url_for("views.home"))
